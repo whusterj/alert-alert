2ae87e6a1d9419c03830e79cf2996dfd1bffcfc9
William Huster <whusterj@gmail.com>
2015-08-30 01:13:32 -0400

    Update to github demo page

--- demo/index.js
+++ demo/index.js
@@ -4,7 +4,7 @@
  * designed to run in modern browsers without external dependencies.
  *
  * @author  William Huster  <whusterj@gmail.com>
- * @version 1.0.8
+ * @version 1.0.9
  *
  * Usage:
  *
@@ -90,8 +90,46 @@ var Alert = require('alert-alert');
 
 console.log('Alert', Alert);
 
-document.getElementById('defaultAlertBtn').addEventListener('click', function () {
-    Alert.alert('default', 'Hello World');
-});
+var genAlertBtn     = document.getElementById('defaultAlertBtn'),
+    infoAlertBtn    = document.getElementById('infoAlertBtn'),
+    successAlertBtn = document.getElementById('successAlertBtn'),
+    warnAlertBtn    = document.getElementById('warningAlertBtn'),
+    errorAlertBtn   = document.getElementById('errorAlertBtn');
+
+genAlertBtn.addEventListener('click', newAlert);
+infoAlertBtn.addEventListener('click', infoAlert);
+successAlertBtn.addEventListener('click', successAlert);
+warnAlertBtn.addEventListener('click', warnAlert);
+errorAlertBtn.addEventListener('click', errorAlert);
+
+function newAlert (type, message, timeout) {
+  var type = type || 'info',
+      message = message || '<p>Boring ol\' default....</p>',
+      config = {
+        timeout: timeout || 7000
+      };
+  
+  // AND HERE'S THE MAGIC:
+  Alert.alert(type, message, config);
+}
+
+function infoAlert () {
+  newAlert('info', '<p>Here\'s some vital info!</p>');
+}
+
+function successAlert () {
+  newAlert('success', '<p>Waaah! Great success!</p>');
+}
+
+function warnAlert () {
+  newAlert('warning', '<p>Ah, ah, ah. I wouldn\'t do that!</p>');
+}
+
+function errorAlert () {
+  newAlert('error', '<p>Woah! Oh noes! Something broke!</p>');
+}
+
+// Syntax highlighting for demo
+hljs.initHighlightingOnLoad();
 
 },{"alert-alert":1}]},{},[2]);
